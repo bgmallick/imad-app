6ce81e0c717fe063c39cfc9d3b224d0744d74d11
bgmallick <33228397+bgmallick@users.noreply.github.com>
2018-02-23 04:12:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var img = document.getElementById('madi');
 var marginLeft = 0;
-function moveRIght (){
+function moveRight (){
     marginLeft+=5;
     img.style.marginLeft = marginLeft + 'px';
 }
