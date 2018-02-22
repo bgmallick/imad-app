cfb0f581f32d5c0d1c9a29fd315a8610ee2fbd3e
bgmallick <33228397+bgmallick@users.noreply.github.com>
2018-02-23 04:11:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRIght (){
+    marginLeft+=5;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function () {
+    var interval = setInterval(moveRight, 50);
+};
